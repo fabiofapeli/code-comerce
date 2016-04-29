7c2d59031305987a4cc5f52077b703b8ced45b74
Fabio Pereira de Lima <fabio.fapeli@gmail.com>
2016-04-28 21:37:40 -0300

    Campo User::is_admin como fillable

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'address', 'number', 'district', 'city', 'state', 'zipcode', 'phone'
+        'name', 'email', 'password', 'address', 'number', 'district', 'city', 'state', 'zipcode', 'phone', 'is_admin'
     ];
 
     /**
